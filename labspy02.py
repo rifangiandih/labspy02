6aa516097b227c69c582f482a3a8fea73f54e8ac
rifangiandih <rifangiandihidayat@gmail.com>
2019-11-02 23:12:16 +0700

    File python

---
+++ labspy02.py
@@ -0,0 +1,14 @@
+def tes():
+    a=int(input("bilangan 1 ="))
+    b=int(input("bilangan 2 ="))
+    c=int(input("bilangan 3 ="))
+
+    if a>b and a>c:
+        print (a, 'terbesar')
+    elif b>a and b>c:
+        print (b, 'terbesar')
+    else:
+        print (c, 'terbesar')
+
+tes()
+    
